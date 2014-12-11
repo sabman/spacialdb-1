ae9ca3b2cc966d83e0ed2a506eef7d40f3c3af92
Kashif Rasul <kashif.rasul@gmail.com>
2014-12-11 15:43:35 +0100

    use rbenv-vars plugin

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/usr/share/nginx/spacialdb'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_files, fetch(:linked_files, []).push('.rbenv-vars')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')

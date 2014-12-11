dff739f06568dcee39ab05f0a645413055b65812
Kashif Rasul <kashif.rasul@gmail.com>
2014-12-11 14:30:54 +0100

    initial capistrano deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@eu1.spacialdb.com}
+role :web, %w{deploy@eu1.spacialdb.com}
+role :db,  %w{deploy@eu1.spacialdb.com}
 
 
 # Extended Server Syntax
@@ -15,8 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+server 'eu1.spacialdb.com', user: 'deploy', roles: %w{web app db}, ssh_options: {forward_agent: true}
 
 # Custom SSH Options
 # ==================
@@ -25,7 +24,7 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
+#set :ssh_options, {
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)

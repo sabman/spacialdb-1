c8830fcf340bfb9e03ebc9b469f3316437eafe8d
Kashif Rasul <kashif.rasul@gmail.com>
2015-08-04 12:58:26 +0200

    updated repo_url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'spacialdb'
-set :repo_url, 'git@github.com:sabman/spacialdb.git'
+set :repo_url, 'git@github.com:nomadlabs/spacialdb.git'
 
 # Default branch is :master
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
